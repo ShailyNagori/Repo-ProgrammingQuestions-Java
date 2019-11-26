8f9bba30f08399fc8446612cad36ecdae1a2e418
ShailyNagori <er_shaily@outlook.com>
2019-11-25 17:50:10 -0800

    Adding solution for Leetcode questions:First Unique Char In a String, and to determine if a linked list is palindrome

---
+++ src/com/codeFactory/test/LC_LLisPalindrome.java
@@ -0,0 +1,58 @@
+package com.codeFactory.test;
+
+/*
+Given a singly linked list, determine if it is a palindrome.
+
+Example 1:
+
+Input: 1->2
+Output: false
+ */
+
+public class LC_LLisPalindrome {
+    public boolean isPalindrome(LC2_Add2Numbers.ListNode head) {
+        if(head == null || head.next == null) return  true;
+        LC2_Add2Numbers.ListNode fast = head;
+        LC2_Add2Numbers.ListNode slow = head;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if(fast != null) {  //Odd number of nodes
+            slow = slow.next; //Let right part be smaller
+        }
+        slow = reverse(slow);
+        fast = head;
+        while (slow != null) {
+            if(fast.val != slow.val) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+    public LC2_Add2Numbers.ListNode reverse(LC2_Add2Numbers.ListNode head) {
+        LC2_Add2Numbers.ListNode next = head;
+        LC2_Add2Numbers.ListNode curr = head;
+        LC2_Add2Numbers.ListNode prev = null;
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        LC_LLisPalindrome obj = new LC_LLisPalindrome();
+        LC2_Add2Numbers.ListNode head = new LC2_Add2Numbers.ListNode(1);
+        head.next = new LC2_Add2Numbers.ListNode(2);
+        head.next.next = new LC2_Add2Numbers.ListNode(1);
+        boolean res = obj.isPalindrome(head);
+        System.out.println(res);
+
+    }
+}
---
+++ src/com/codeFactory/test/LC_firstUniqChar.java
@@ -0,0 +1,39 @@
+package com.codeFactory.test;
+/*
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+ */
+
+public class LC_firstUniqChar {
+    private int firstUniqChar(String s) {
+        int[] freq = new int[26];
+        int[] loc = new int[26];
+        int n = s.length();
+        int ret = -1;
+        for(int i = 0 ; i< n; i++) {
+            freq[s.charAt(i) - 'a']++;
+            loc[s.charAt(i) - 'a'] = i;
+
+        }
+        for(int i = 0; i< 26; i++) {
+            if(freq[i] == 1) {
+                ret = ret == -1 ? loc[i] : Math.min(ret, loc[i]);
+            }
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        LC_firstUniqChar obj = new LC_firstUniqChar();
+        String s = "loveleetcode";
+        int res = obj.firstUniqChar(s);
+        System.out.println(res);
+    }
+}
